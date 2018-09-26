4786453af26d6c987ce473ef621567bfdefa6f76
Андрей Забавко <zeba.brat@Zebabrat.local>
2018-09-27 02:11:09 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,28 @@
 module.exports = function getZerosCount(number, base) {
-  // your implementation
+  let zerosCount = number;
+  let degreeOfMultiplier;
+  let numberOfZerosOfMultiplier;
+
+  for (var i = 2; i <= base; i++) {
+    if (base % i === 0) {
+      degreeOfMultiplier = 0;   // multipliers degree
+
+      while (base % i === 0) {   // finding multiplier
+        base = Math.floor(base / i);
+        degreeOfMultiplier++;
+      }
+
+      let numberOfMultiplier = number;
+      numberOfZerosOfMultiplier = 0; // count zeros
+
+      while (numberOfMultiplier) {
+        numberOfZerosOfMultiplier += Math.floor(numberOfMultiplier / i);
+        numberOfMultiplier = Math.floor(numberOfMultiplier / i);
+      }
+
+      let result = Math.floor(numberOfZerosOfMultiplier / degreeOfMultiplier);
+      zerosCount = Math.min(zerosCount, result);
+    }
+  }
+  return zerosCount;
 }
